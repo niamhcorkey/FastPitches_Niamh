3645aab4be5ceb14e1af0714fd0ee224dca75d84
Niamh Corkey <s1936986@ed.ac.uk>
2022-11-22 14:25:25 +0000

    New input files with column for coefficients

--- PyTorch/SpeechSynthesis/FastPitch/newinputfiles.py
+++ PyTorch/SpeechSynthesis/FastPitch/newinputfiles.py
@@ -1,15 +1,20 @@
 
 
 def add_column(filepath, newtype):
-    newfile = open(f"new{newtype}input.txt", 'w')
-    newfile = open(f"new{newtype}input.txt", 'a')
+    open(f"filelists/ljs_audio_pitch_coefs_text_{newtype}.txt", 'w')
+    newfile = open(f"filelists/ljs_audio_pitch_coefs_text_{newtype}.txt", 'a')
 
     with open(filepath, 'r') as inputfile:
         for line in inputfile:
-            fileno = line[5:15]
-            end = line[40:]
-            newline = line[0:40] + f"new/{fileno}.txt|" + line[40:]
-            newfile.write(newline)
 
-add_column("filelists/ljs_audio_pitch_text_val.txt", "val")
+            if len(line) > 20:
+                fileno = line[5:15]
+                newline = line[0:40] + f"coefs/{fileno}.txt|" + line[40:]
+                newfile.write(newline)
+
+            else:
+                newline = "mels|pitch|coefs|text\n"
+                newfile.write(newline)
+
+add_column("filelists/ljs_audio_pitch_text_test.txt", "test")
 
