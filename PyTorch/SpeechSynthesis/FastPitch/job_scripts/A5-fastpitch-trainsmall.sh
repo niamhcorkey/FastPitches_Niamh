6fcd91706e1b1b6f22453ca6eab74d086f1291c3
Niamh Corkey <s1936986@ed.ac.uk>
2022-12-05 23:34:33 +0000

    Trial training scripts

--- PyTorch/SpeechSynthesis/FastPitch/job_scripts/A5-fastpitch-trainsmall.sh
+++ PyTorch/SpeechSynthesis/FastPitch/job_scripts/A5-fastpitch-trainsmall.sh
@@ -32,8 +32,9 @@ FP=$DS_HOME/FastPitches_Niamh/PyTorch/SpeechSynthesis/FastPitch
 # some values are set by the queuing software, e.g. $JOB_ID
 # -- see `man qsub` and search for 'ENVIRONMENT VARIABLES'
 
-export OUTPUT_DIR=$DS_HOME/trained_models/${JOB_NAME}
+export OUTPUT_DIR=$DS_HOME/trained_models/$1
 export DATASET_PATH=$DS_HOME/LJSpeech-1.1
+echo ${DATASET_PATH}
 # if running after A2-fastpitch-prepare-lj-data.sh and with
 # LOAD_PITCH_FROM_DISK=true below, use ljs_audio_pitch_text_*.txt files
 # which point to saved pitch contours. If extracting pitches from audio
