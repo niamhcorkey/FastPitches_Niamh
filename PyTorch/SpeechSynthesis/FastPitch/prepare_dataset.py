1e6d032dd8ca49f97546d2f1c8a208640d57f1ca
Niamh Corkey <s1936986@ed.ac.uk>
2023-01-15 18:23:53 +0000

    Extracting mels and pitch from chopped data

--- PyTorch/SpeechSynthesis/FastPitch/prepare_dataset.py
+++ PyTorch/SpeechSynthesis/FastPitch/prepare_dataset.py
@@ -142,7 +142,7 @@ def main():
         for i, batch in enumerate(tqdm.tqdm(data_loader)):
             tik = time.time()
 
-            _, input_lens, mels, mel_lens, _, pitch, _, _, attn_prior, fpaths = batch
+            _, input_lens, mels, mel_lens, _, pitch, _, _, attn_prior, fpaths, _ = batch
 
             # Ensure filenames are unique
             for p in fpaths:
