e61ea5192c85633e98334202606b39c698211b2f
Niamh Corkey <s1936986@ed.ac.uk>
2023-01-15 17:44:40 +0000

    Extracting mels and pitch from chopped data

---
+++ PyTorch/SpeechSynthesis/FastPitch/job_scripts/A7-fastpitch-prepare-lj-data-chopped.sh
@@ -0,0 +1,44 @@
+#!/bin/sh
+#
+# grid engine options
+#$ -N fastpitch_prep_lj
+#$ -l h_rt=24:00:00
+#$ -l h_vmem=1G
+#$ -pe sharedmem 8
+#$ -R y
+#$ -o /exports/chss/eddie/ppls/groups/lel_hcrc_cstr_students/s1936986_Niamh_Corkey/job_logs/$JOB_NAME_$JOB_ID.stdout
+#$ -e /exports/chss/eddie/ppls/groups/lel_hcrc_cstr_students/s1936986_Niamh_Corkey/job_logs/$JOB_NAME_$JOB_ID.stderr
+#$ -M s1936986@ed.ac.uk
+#$ -m beas
+
+# initialise environment modules
+. /etc/profile.d/modules.sh
+
+module load anaconda
+source activate fastpitch1
+
+set -euo pipefail
+
+UUN=s1936986
+YOUR_NAME=Niamh_Corkey
+
+DS_HOME=/exports/chss/eddie/ppls/groups/lel_hcrc_cstr_students/${UUN}_${YOUR_NAME}
+FP=$DS_HOME/FastPitches_Niamh/PyTorch/SpeechSynthesis/FastPitch
+
+SCRATCH=/exports/eddie/scratch/s1936986
+DATA_DIR="$DS_HOME/LJ_Chopped"
+
+cd $FP
+
+# have to set smaller --n-workers than $FP/scripts/prepare_dataset.sh
+# to work around weird qsub memory consumption
+python prepare_dataset.py \
+    --dataset-path $DATA_DIR \
+    --wav-text-filelists $DATA_DIR/chopped_filelist.txt \
+    --n-workers 1 \
+    --batch-size 1 \
+    --extract-pitch \
+    --extract-mels
+    # NB: this has to use `--batch-size 1` otherwise archives get saved with
+    # padding and everything ends up the wrong shape!
+
