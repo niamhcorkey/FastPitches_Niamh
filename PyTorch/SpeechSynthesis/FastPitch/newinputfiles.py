2ad63a22307fb7e82a16fd2a64a0d3612bd9795e
Niamh Corkey <s1936986@ed.ac.uk>
2023-01-25 16:42:39 +0000

    New train script

--- PyTorch/SpeechSynthesis/FastPitch/newinputfiles.py
+++ PyTorch/SpeechSynthesis/FastPitch/newinputfiles.py
@@ -1,17 +1,4 @@
 
-
-def add_column(filepath, newtype):
-    newfile = open(f"new{newtype}input.txt", 'w')
-    newfile = open(f"new{newtype}input.txt", 'a')
-
-    with open(filepath, 'r') as inputfile:
-        for line in inputfile:
-            fileno = line[5:15]
-            end = line[40:]
-            newline = line[0:40] + f"new/{fileno}.txt|" + line[40:]
-            newfile.write(newline)
-
-
 import re
 
 def abs_path(og, type):
