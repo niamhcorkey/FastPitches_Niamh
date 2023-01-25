3958125cdf5d6d97ba05c35da6e3bb8b78ad0aee
Niamh Corkey <s1936986@ed.ac.uk>
2023-01-25 16:41:14 +0000

    New train script

--- PyTorch/SpeechSynthesis/FastPitch/newinputfiles.py
+++ PyTorch/SpeechSynthesis/FastPitch/newinputfiles.py
@@ -11,5 +11,23 @@ def add_column(filepath, newtype):
             newline = line[0:40] + f"new/{fileno}.txt|" + line[40:]
             newfile.write(newline)
 
-add_column("filelists/ljs_audio_pitch_text_val.txt", "val")
+
+import re
+
+def abs_path(og, type):
+    open(f"abs_main_{type}.txt", 'w')
+    newfile = open(f"abs_main_{type}.txt", 'a')
+    newfile.write("mels|pitch|text\n")
+    line_re = re.compile(r'(.*)\|(.*)\|(.*)')
+    abspath = "/disk/scratch1/s1936986/LJSpeech-1.1/"
+    with open(og, 'r') as f:
+        for line in f:
+            match = line_re.match(line)
+            wav = match.group(1)
+            pitch = match.group(2)
+            text = match.group(3)
+            newline = abspath + wav + "|" + abspath + pitch + "|" +  text +"\n"
+            newfile.write(newline)
+
+abs_path("ljs_audio_pitch_text_val.txt", "val")
 
