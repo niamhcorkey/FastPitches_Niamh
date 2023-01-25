6e869e6afc17f34de10bde3e54ad7ae864dd4609
Niamh Corkey <s1936986@ed.ac.uk>
2023-01-25 17:08:27 +0000

    New train script

--- PyTorch/SpeechSynthesis/FastPitch/newinputfiles.py
+++ PyTorch/SpeechSynthesis/FastPitch/newinputfiles.py
@@ -2,20 +2,20 @@
 import re
 
 def abs_path(og, type):
-    open(f"abs_main_{type}.txt", 'w')
-    newfile = open(f"abs_main_{type}.txt", 'a')
+    open(f"filelists/abs_main_{type}.txt", 'w')
+    newfile = open(f"filelists/abs_main_{type}.txt", 'a')
     newfile.write("mels|pitch|text\n")
-    line_re = re.compile(r'(.*)\|(.*)\|(.*)')
+    line_re = re.compile(r'wavs/(.*)\.wav\|(.*)\|(.*)')
     abspath = "/disk/scratch1/s1936986/LJSpeech-1.1/"
     with open(og, 'r') as f:
         for line in f:
             if not line.startswith("m"):
                 match = line_re.match(line)
-                wav = match.group(1)
+                base = match.group(1)
                 pitch = match.group(2)
                 text = match.group(3)
-                newline = abspath + wav + "|" + abspath + pitch + "|" +  text +"\n"
+                newline = abspath + f"mels/{base}.pt" + "|" + abspath + pitch + "|" +  text +"\n"
                 newfile.write(newline)
 
-abs_path("ljs_audio_pitch_text_train_v3.txt", "train")
+abs_path("filelists/ljs_audio_pitch_text_val.txt", "val")
 
