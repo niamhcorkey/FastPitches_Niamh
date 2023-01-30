93cc5fd81e7e274781bf1cba99ce1bd65c53e240
Niamh Corkey <s1936986@ed.ac.uk>
2023-01-30 10:38:27 +0000

    Coefficient targets

--- PyTorch/SpeechSynthesis/FastPitch/fastpitch/model.py
+++ PyTorch/SpeechSynthesis/FastPitch/fastpitch/model.py
@@ -425,7 +425,7 @@ class FastPitch(nn.Module):
                 coef_emb = self.coefficient_emb(masked_coef_pred).permute(0, 2, 1)
             else:
                 print(coef_tgt)
-                coef_tgt = torch.stack(coef_tgt,1)
+                coef_tgt = torch.stack(coef_tgt,0)
                 print(coef_tgt)
                 print(coef_tgt.size())
                 coef_tgt_ups = coef_tgt.unsqueeze(-1)
