ab90b8c96c510f615e7dfee343c0d3d045f09494
Niamh Corkey <s1936986@ed.ac.uk>
2023-02-01 20:43:54 +0000

    Inference phrases

--- PyTorch/SpeechSynthesis/FastPitch/coeftargets/savetgt.py
+++ PyTorch/SpeechSynthesis/FastPitch/coeftargets/savetgt.py
@@ -1,8 +1,8 @@
 import numpy as np
 
-tgt = [-3, -3, -3]
+tgt = [-1.47776392, 416835, 0.42459683]
 tgt = np.array(tgt)
-#np.save("n333", tgt)
+np.save("real", tgt)
 
-coefs = np.load("/Users/user/diss/LJ_Chopped/coefs_norm/LJ007-0053b.npy")
-print(coefs)
+#coefs = np.load("/Users/user/diss/LJ_Chopped/coefs_norm/LJ007-0053b.npy")
+#print(coefs)
