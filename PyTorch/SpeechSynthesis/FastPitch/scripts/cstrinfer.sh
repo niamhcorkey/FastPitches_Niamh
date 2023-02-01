631489b02ac4575bb5dc349f25bbf054f1da2979
Niamh Corkey <s1936986@ed.ac.uk>
2023-02-01 20:58:36 +0000

    Inference phrases

--- PyTorch/SpeechSynthesis/FastPitch/scripts/cstrinfer.sh
+++ PyTorch/SpeechSynthesis/FastPitch/scripts/cstrinfer.sh
@@ -14,12 +14,12 @@ FP=${DS_HOME}/FastPitches_Niamh/PyTorch/SpeechSynthesis/FastPitch
 MODEL=fullutts2601
 CHECKPOINT=FastPitch_checkpoint_400.pt
 
-NAME=coeftestutts
+NAME=quotes
 
 : ${WAVEGLOW:="pretrained_models/waveglow/nvidia_waveglow256pyt_fp16.pt"}
 : ${FASTPITCH:="$DS_HOME/trained_models/$MODEL/$CHECKPOINT"}
 : ${BATCH_SIZE:=16}
-: ${PHRASES:="phrases/coeftest.tsv"}
+: ${PHRASES:="phrases/pred.tsv"}
 : ${OUTPUT_DIR:="$DS_HOME/fastpitch_audio/$(basename $NAME .tsv)"}
 : ${LOG_FILE:="$OUTPUT_DIR/nvlog_infer.json"}
 : ${AMP:=false}
@@ -37,7 +37,7 @@ NAME=coeftestutts
 # Enable coefficient conditioning
 : ${COEFFICIENTS:=true}
 # Load in coefficient targets (for some reason true is false and false is true?)
-: ${USE_COEF_TARGET:=true}
+: ${USE_COEF_TARGET:=false}
 
 
 
