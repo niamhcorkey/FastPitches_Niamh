fdefeae2e990b1c19c819407c81696f6f7cf7fd4
Niamh Corkey <s1936986@ed.ac.uk>
2023-02-02 14:39:39 +0000

    Inference

--- PyTorch/SpeechSynthesis/FastPitch/scripts/cstrinfer.sh
+++ PyTorch/SpeechSynthesis/FastPitch/scripts/cstrinfer.sh
@@ -11,15 +11,15 @@ set -euo pipefail
 DS_HOME=/disk/scratch1/s1936986
 FP=${DS_HOME}/FastPitches_Niamh/PyTorch/SpeechSynthesis/FastPitch
 
-MODEL=baseline2601
-CHECKPOINT=FastPitch_checkpoint_1000.pt
+MODEL=fullutts2601
+CHECKPOINT=FastPitch_checkpoint_540.pt
 
-NAME=quotes
+NAME=preds
 
 : ${WAVEGLOW:="pretrained_models/waveglow/nvidia_waveglow256pyt_fp16.pt"}
 : ${FASTPITCH:="$DS_HOME/trained_models/$MODEL/$CHECKPOINT"}
 : ${BATCH_SIZE:=16}
-: ${PHRASES:="phrases/pred.tsv"}
+: ${PHRASES:="phrases/devset10.tsv"}
 : ${OUTPUT_DIR:="$DS_HOME/fastpitch_audio/$(basename $NAME .tsv)"}
 : ${LOG_FILE:="$OUTPUT_DIR/nvlog_infer.json"}
 : ${AMP:=false}
