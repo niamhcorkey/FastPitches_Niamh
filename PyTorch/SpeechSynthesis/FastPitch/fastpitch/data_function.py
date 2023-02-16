582147fa2d1640624627e99f7d4ecfa5fa557f79
Niamh Corkey <s1936986@ed.ac.uk>
2023-02-16 21:14:50 +0000

    Allow to change no of coefs

--- PyTorch/SpeechSynthesis/FastPitch/fastpitch/data_function.py
+++ PyTorch/SpeechSynthesis/FastPitch/fastpitch/data_function.py
@@ -412,7 +412,6 @@ class TTSCollate:
         audiopaths = [batch[i][7] for i in ids_sorted_decreasing]
 
         if batch[0][8] is not None:
-            print(f"SIZE OF THING IN BATCH: {len(batch[0][8])}")
             n_coefs = len(batch[0][8])
             coefs_padded = torch.zeros(mel_padded.size(0), n_coefs, dtype=batch[0][8].dtype)
 
