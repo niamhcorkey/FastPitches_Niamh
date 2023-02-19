de43518f6f007a50d0e4f6e85da035ecd2fb2385
Niamh Corkey <s1936986@ed.ac.uk>
2023-02-19 17:16:54 +0000

    New coef tgts

--- PyTorch/SpeechSynthesis/FastPitch/scripts/cstrinfer.sh
+++ PyTorch/SpeechSynthesis/FastPitch/scripts/cstrinfer.sh
@@ -14,12 +14,12 @@ FP=${DS_HOME}/FastPitches_Niamh/PyTorch/SpeechSynthesis/FastPitch
 MODEL=phrasesphones
 CHECKPOINT=FastPitch_checkpoint_1000.pt
 
-NAME=utt9
+NAME=utt10
 
 : ${WAVEGLOW:="pretrained_models/waveglow/nvidia_waveglow256pyt_fp16.pt"}
 : ${FASTPITCH:="$DS_HOME/trained_models/$MODEL/$CHECKPOINT"}
 : ${BATCH_SIZE:=16}
-: ${PHRASES:="phrases/utt9_continuum.tsv"}
+: ${PHRASES:="phrases/utt10_continuum.tsv"}
 : ${OUTPUT_DIR:="$DS_HOME/fastpitch_audio/continuum/$(basename $NAME .tsv)"}
 : ${LOG_FILE:="$OUTPUT_DIR/nvlog_infer.json"}
 : ${AMP:=false}
