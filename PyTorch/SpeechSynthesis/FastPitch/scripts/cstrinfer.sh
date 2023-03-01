69f50ecfaf6cf5b3d498904a1311d8b3d2012ba0
Niamh Corkey <s1936986@ed.ac.uk>
2023-03-01 16:29:35 +0000

    Full utts controllability test

--- PyTorch/SpeechSynthesis/FastPitch/scripts/cstrinfer.sh
+++ PyTorch/SpeechSynthesis/FastPitch/scripts/cstrinfer.sh
@@ -11,16 +11,16 @@ set -euo pipefail
 DS_HOME=/disk/scratch1/s1936986
 FP=${DS_HOME}/FastPitches_Niamh/PyTorch/SpeechSynthesis/FastPitch
 
-MODEL=phrasesphones
+MODEL=fulluttsphones
 CHECKPOINT=FastPitch_checkpoint_1000.pt
 
-NAME=listening
+NAME=continuum
 
 : ${WAVEGLOW:="pretrained_models/waveglow/nvidia_waveglow256pyt_fp16.pt"}
 : ${FASTPITCH:="$DS_HOME/trained_models/$MODEL/$CHECKPOINT"}
 : ${BATCH_SIZE:=16}
-: ${PHRASES:="phrases/predictions.tsv"}
-: ${OUTPUT_DIR:="$DS_HOME/fastpitch_audio/phrasescontrollabilitytest/$(basename $NAME .tsv)"}
+: ${PHRASES:="phrases/fullutts_continuum.tsv"}
+: ${OUTPUT_DIR:="$DS_HOME/fastpitch_audio/fulluttscontrollabilitytest/$(basename $NAME .tsv)"}
 : ${LOG_FILE:="$OUTPUT_DIR/nvlog_infer.json"}
 : ${AMP:=false}
 : ${TORCHSCRIPT:=false}
@@ -37,7 +37,7 @@ NAME=listening
 # Enable coefficient conditioning
 : ${COEFFICIENTS:=true}
 # Load in coefficient targets
-: ${USE_COEF_TARGET:=false}
+: ${USE_COEF_TARGET:=true}
 : ${NCOEFFICIENTS:=3}
 
 
