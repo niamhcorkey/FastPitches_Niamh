a6df79d05fc41c88ad083ad5807d3452694349da
Niamh Corkey <s1936986@ed.ac.uk>
2023-03-11 17:52:08 +0000

    Naturalness inference

--- PyTorch/SpeechSynthesis/FastPitch/scripts/cstrinfer.sh
+++ PyTorch/SpeechSynthesis/FastPitch/scripts/cstrinfer.sh
@@ -11,10 +11,10 @@ set -euo pipefail
 DS_HOME=/disk/scratch1/s1936986
 FP=${DS_HOME}/FastPitches_Niamh/PyTorch/SpeechSynthesis/FastPitch
 
-MODEL=fulluttsphones
+MODEL=phrasesphones
 CHECKPOINT=FastPitch_checkpoint_1000.pt
 
-NAME=coefsfullutts
+NAME=coefsphrases
 
 : ${WAVEGLOW:="pretrained_models/waveglow/nvidia_waveglow256pyt_fp16.pt"}
 : ${FASTPITCH:="$DS_HOME/trained_models/$MODEL/$CHECKPOINT"}
