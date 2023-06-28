3df714829e532982353f58a5f3727d13fe321744
Niamh Corkey <s1936986@ed.ac.uk>
2023-06-28 12:57:59 +0100

    Prepare dataset alba

--- PyTorch/SpeechSynthesis/FastPitch/prepare_dataset.py
+++ PyTorch/SpeechSynthesis/FastPitch/prepare_dataset.py
@@ -84,11 +84,8 @@ def main():
     parser = argparse.ArgumentParser(description='FastPitch Data Pre-processing')
     parser = parse_args(parser)
     args, unk_args = parser.parse_known_args()
-    print(args)
-    print(unk_args)
     if len(unk_args) > 0:
-        print('PROBLEM')
-        raise ValueError(f'Invalid options {unk_args}')
+        raise ValueError(f"Invalid options {unk_args}")
 
     DLLogger.init(backends=[JSONStreamBackend(Verbosity.DEFAULT, Path(args.dataset_path, args.log_file)),
                             StdOutBackend(Verbosity.VERBOSE)])
