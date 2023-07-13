cf8b3bebbf11fcf2bb0fa47148e9d9d446e882f0
Niamh Corkey <s1936986@ed.ac.uk>
2023-07-13 15:33:14 +0100

    Changing input symbols to phones

--- PyTorch/SpeechSynthesis/FastPitch/common/text/text_processing.py
+++ PyTorch/SpeechSynthesis/FastPitch/common/text/text_processing.py
@@ -194,7 +194,13 @@ class PhoneProcessor(object):
             # Assuming space-delimited phone strings, e.g. 'sp D @ k { t sp'
             print(text)
             print([s for s in text.split(' ')])
-            return [self.symbol_to_id[s] for s in text.split(' ')]
+            for s in text.split(' '):
+                try:
+                    self.symbol_to_id[s]
+                except KeyError:
+                    print(f"'{text}'")
+                    exit
+            #return [self.symbol_to_id[s] for s in text.split(' ')]
 
     def ids_to_text(self, ids):
             return ' '.join(self.id_to_symbol[i] for i in ids)
