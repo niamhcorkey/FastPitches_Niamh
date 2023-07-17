9d5819e895191d35357a4f6f205724b044f07478
Niamh Corkey <s1936986@ed.ac.uk>
2023-07-17 20:14:30 +0100

    Inference

--- PyTorch/SpeechSynthesis/FastPitch/scripts/cstrinfer.sh
+++ PyTorch/SpeechSynthesis/FastPitch/scripts/cstrinfer.sh
@@ -14,12 +14,12 @@ FP=${DS_HOME}/FastPitches_Niamh/PyTorch/SpeechSynthesis/FastPitch
 MODEL=newsymbols
 CHECKPOINT=FastPitch_checkpoint_1000.pt
 
-NAME=newsymbols2
+NAME=newsymbolstest
 
 : ${WAVEGLOW:="pretrained_models/waveglow/nvidia_waveglow256pyt_fp16.pt"}
 : ${FASTPITCH:="$DS_HOME/sgile/trained_models/$MODEL/$CHECKPOINT"}
 : ${BATCH_SIZE:=6}
-: ${PHRASES:="phrases/booksphones.tsv"}
+: ${PHRASES:="phrases/testphones.tsv"}
 : ${OUTPUT_DIR:="$DS_HOME/sgile/fastpitch_audio/$(basename $NAME .tsv)"}
 : ${LOG_FILE:="$OUTPUT_DIR/nvlog_infer.json"}
 : ${AMP:=false}
