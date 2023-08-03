a78bb03fce6c262958c5a368b1bc41c7c3f2826f
Niamh Corkey <s1936986@ed.ac.uk>
2023-08-03 13:09:30 +0100

    LJ transfer test

--- PyTorch/SpeechSynthesis/FastPitch/scripts/cstrinfer.sh
+++ PyTorch/SpeechSynthesis/FastPitch/scripts/cstrinfer.sh
@@ -14,12 +14,12 @@ FP=${DS_HOME}/FastPitches_Niamh/PyTorch/SpeechSynthesis/FastPitch
 MODEL=phrasesphones
 CHECKPOINT=FastPitch_checkpoint_1000.pt
 
-NAME=transfertest1
+NAME=transfertest2
 
 : ${WAVEGLOW:="pretrained_models/waveglow/nvidia_waveglow256pyt_fp16.pt"}
 : ${FASTPITCH:="$DS_HOME/trained_models/$MODEL/$CHECKPOINT"}
 : ${BATCH_SIZE:=16}
-: ${PHRASES:="phrases/transfer.tsv"}
+: ${PHRASES:="phrases/transfer2.tsv"}
 : ${OUTPUT_DIR:="$DS_HOME/fastpitch_audio/conference/$(basename $NAME .tsv)"}
 : ${LOG_FILE:="$OUTPUT_DIR/nvlog_infer.json"}
 : ${AMP:=false}
